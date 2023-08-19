e547f542897a279603b2255cb026a2e81f30069b
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 01:59:34 -0400

    Added comments to WasteSortingGame.java

--- WasteSortingGame.java
+++ WasteSortingGame.java
@@ -7,64 +7,22 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+// This class represents the main game window for the Waste Sorting Game.
 public class WasteSortingGame extends JFrame {
-    private WSGame game;
-    private GamePanel gamePanel;
-    private static final int INTERVAL = 5;
-    private Timer timer;
-    private ScorePanel scorePanel;
+    private WSGame game; // The game logic
+    private GamePanel gamePanel; // The panel for rendering the game
+    private static final int INTERVAL = 5; // Timer interval in milliseconds
+    private Timer timer; // Timer for updating the game
+    private ScorePanel scorePanel; // Panel for displaying the score
 
-    // Constructs main window
-    // effects: sets up window in which Space Invaders game will be played
-    public  WasteSortingGame() {
-        super("Waste Sorting Game");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game = new WSGame();
-        gamePanel = new GamePanel(game);
-        scorePanel = new ScorePanel(game);
-        add(gamePanel);
-        add(scorePanel, BorderLayout.NORTH);
-        pack();
-        addKeyListener(new KeyHandler());
-        centreOnScreen();
-        setVisible(true);
-        addTimer();
-        timer.start();
-        game.addObserver(scorePanel);
-}
-
-    // switch to the given panel
-
-    private void centreOnScreen() {
-        Dimension scrn = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation((scrn.width - getWidth()) / 2, (scrn.height - getHeight()) / 2);
-    }
-
-    /*
-     * A key handler to respond to key events
-     */
-    private class KeyHandler extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-            game.keyPressed(e.getKeyCode());
-        }
-    }
-
-    // Set up timer
-    // modifies: none
-    // effects:  initializes a timer that updates game each
-    //           INTERVAL milliseconds
-    private void addTimer() {
-        timer = new Timer(INTERVAL, ae -> {
-            game.update(this);
-            gamePanel.repaint();
-        });
-    }
-    public void stopTimer() {
-        timer.stop();
-    }
-
-    public static void main(String[] args) {
-        new InstructionFrame();
-    }
-}
+    // Constructor
+    public WasteSortingGame() {
+        super("Waste Sorting Game"); // Set window title
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set default close operation
+        game = new WSGame(); // Initialize the game logic
+        gamePanel = new GamePanel(game); // Create the game panel
+        scorePanel = new ScorePanel(game); // Create the score panel
+        add(gamePanel); // Add the game panel to the frame
+        add(scorePanel, BorderLayout.NORTH); // Add the score panel to the top of the frame
+        pack(); // Pack the components
+        addKeyListener(new KeyHandler()); //
